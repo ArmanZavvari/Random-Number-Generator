0935843c1c6eb1ba6cd0c4980b8338cff9a0b525
Arman Zavvari <armanzavvari@gmail.com>
2023-09-21 11:04:22 +0330

    Add generateRandomNaturalNumberWithExclusions function

--- randomNumber.ts
+++ randomNumber.ts
@@ -1,13 +1,26 @@
-function generateRandomNumber(min, max) {
-    if (min >= max) {
-      throw new Error("Invalid range. 'min' must be less than 'max'.");
-    }
-    return Math.random() * (max - min) + min;
+function generateRandomNaturalNumberWithExclusions(min, max, exclusions) {
+  if (min >= max || min < 0) {
+    throw new Error("Invalid range. 'min' must be a non-negative number less than 'max'.");
   }
-  
-  // Example usage:
-  const min = 1;
-  const max = 100;
-  const randomNumber = generateRandomNumber(min, max);
-  console.log(`Random number between ${min} and ${max}: ${randomNumber}`);
-  
+
+  const availableNumbers = [];
+  for (let i = min; i < max; i++) {
+    if (!exclusions.includes(i)) {
+      availableNumbers.push(i);
+    }
+  }
+
+  if (availableNumbers.length === 0) {
+    throw new Error("No available numbers within the specified range after exclusions.");
+  }
+
+  const randomIndex = Math.floor(Math.random() * availableNumbers.length);
+  return availableNumbers[randomIndex];
+}
+
+// Example usage:
+const min = 1; // Minimum value (inclusive)
+const max = 100; // Maximum value (exclusive)
+const exclusions = [5, 10, 15]; // Numbers to exclude
+const randomNaturalNumber = generateRandomNaturalNumberWithExclusions(min, max, exclusions);
+console.log(`Random natural number between ${min} and ${max} (excluding ${exclusions.join(', ')}): ${randomNaturalNumber}`);
